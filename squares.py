c96a76f87305bffe30a3575bad898f95a8e94a09
Rita Kushwaha <>
2021-02-27 23:00:04 +0530

    import functions.square(i)

---
+++ squares.py
@@ -0,0 +1,8 @@
+#from functions import square
+import functions
+
+#for i in range(10):
+    #print(f"Square of {i} is {square(i)}")
+
+for i in range(10):
+    print(f"Square of {i} is {functions.square(i)}")
