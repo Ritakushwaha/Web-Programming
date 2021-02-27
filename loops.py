befd4b3bc6eace90ee3e2932f8051185e58e9b17
Rita Kushwaha <>
2021-02-27 16:27:03 +0530

    Loops

---
+++ loops.py
@@ -0,0 +1,22 @@
+#looping lists
+for i in [0, 1, 2, 3, 4, 5]:
+    print(i)
+
+#looping with range func
+for i in range(6):
+    print(i)
+
+#looping lists of name
+for i in ["Rita", "Girwar", "Priyanshi", "Pragya", "Vijaya"]:
+    print(i)
+
+#loop through each character in name
+for i in "Rita":
+    print(i)
+
+#loop through dictionaries
+houses = {"Red House" : "Rita", "Blue House" : "Girwar"}
+#change value to dictionary
+print(houses)
+houses["Red House"] = "Priyanshi"
+print(houses)
